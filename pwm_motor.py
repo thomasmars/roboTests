3cdb65a10ce167d300906fbba222ed15b0004827
Thomas Marstrander <thomasmars87@gmail.com>
2016-04-16 14:25:10 +0200

    Figured out duty cycles and frequencies.

--- pwm_motor.py
+++ pwm_motor.py
@@ -2,48 +2,41 @@ import RPi.GPIO as GPIO
 import time
 
 MOTOR_1A = 11
-MOTOR_1B = 13
+# MOTOR_1B = 13
 MOTOR_2A = 3
-MOTOR_2B = 5
+# MOTOR_2B = 5
 
 print "Configuring..."
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(MOTOR_1A, GPIO.OUT)
-GPIO.setup(MOTOR_1B, GPIO.OUT)
+# GPIO.setup(MOTOR_1B, GPIO.OUT)
 GPIO.setup(MOTOR_2A, GPIO.OUT)
-GPIO.setup(MOTOR_2B, GPIO.OUT)
+# GPIO.setup(MOTOR_2B, GPIO.OUT)
 
-POWER_1A = GPIO.PWM(MOTOR_1A, 10)
-POWER_1B = GPIO.PWM(MOTOR_1B, 10)
-POWER_2A = GPIO.PWM(MOTOR_2A, 10)
-POWER_2B = GPIO.PWM(MOTOR_2B, 10)
+POWER_1A = GPIO.PWM(MOTOR_1A, 100)
+POWER_2A = GPIO.PWM(MOTOR_2A, 100)
 
-
-def set_output(motor, boolean):
-    GPIO.output(motor, boolean)
-
-
-def test_motor(motor):
-    print "PIN" + str(motor) + " TRUE"
-    set_output(motor, True)
-    time.sleep(3)
-    print "PIN" + str(motor) + " FALSE"
-    set_output(motor, False)
-    time.sleep(3)
-
-
-# Not working yet
-def test_power(power, duty1, duty2):
-    print "POWER" + str(power) + " TRUE"
-    power.start(duty1)
-    time.sleep(3)
-    print "PIN" + str(power) + " FALSE"
-    power.ChangeDutyCycle(duty2)
-    time.sleep(3)
-    print "stop!"
-
-test_power(POWER_1A, 10, 100)
-test_power(POWER_2A, 10, 100)
+print("Testing motor " + str(MOTOR_1A))
+# test_power(POWER_1A, 20, 50)
+print "80% POWER"
+POWER_1A.start(20)
+POWER_2A.start(20)
+time.sleep(2)
+print "50% POWER"
+POWER_1A.ChangeDutyCycle(50)
+POWER_2A.ChangeDutyCycle(50)
+time.sleep(2)
+print "0% POWER"
+POWER_1A.ChangeDutyCycle(100)
+POWER_2A.ChangeDutyCycle(100)
+time.sleep(2)
+print "100% POWER"
+POWER_1A.ChangeDutyCycle(0)
+POWER_2A.ChangeDutyCycle(0)
+time.sleep(2)
+print "Stop!"
+POWER_1A.stop()
+POWER_2A.stop()
 
 print "GPIO cleanup"
 GPIO.cleanup()
